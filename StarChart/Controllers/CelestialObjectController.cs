5ace2d45743d4a36d230db6df959fdffb7371eeb
Jaime Alvarez <jaimealv994@gmail.com>
2021-11-15 21:18:32 -0600

    inherit from ControllerBase

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,7 +4,7 @@ using StarChart.Data;
 namespace StarChart.Controllers
 {
     [Route(""), ApiController]
-    public class CelestialObjectController : Controller
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
